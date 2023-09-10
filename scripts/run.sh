3c3d8cad25eeefe4d33d8d6b5a7cea6352234a2c
JNSFilipe <jose.filipe@ieee.org>
2023-09-10 22:49:55 +0100

    Changed default background colour

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@ xrandr -s 1920x1080 -r 60  # Set resolution and refresh rate (run xrandr for mor
 #xbacklight -set 10 &
 brightnessctl -d 'amdgpu_bl0' set 30%
 # feh --bg-fill ~/.config/scripts/wallpapers/maroon-cat.png &
-hsetroot -solid "#F38BA8"  # Set solid colour as background 
+hsetroot -solid "#CBA6F7"  # Set solid colour as background 
 xset r rate 200 50 &       # Set kbd holding delay to 200ms, then produce 50 samples/second
 picom &
 
